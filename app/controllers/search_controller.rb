81d67e442a27887a9953600356663d8c155316ec
alexshook <alexandra.shook@gmail.com>
2014-04-03 13:47:45 -0400

    latest search form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,20 +2,14 @@ class SearchController < ApplicationController
 
   def index
   	@background = background_picker
-
-
-
   end
 
   def show
-    @artist = params[:artist]
-    @artist.gsub(' ', '+')
+    @artist = params[:artist].gsub(' ', '+')
     artist_search = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=#{@artist}&api_key=dc6zaTOxFJmzC")
-    @gif_url = artist_search["data"][0]["images"]["original"]["url"]
+    @random_gif = artist_search["data"].sample
+    @gif_url = artist_search["data"]["#{@random_gif}"]["images"]["original"]["url"]
   end
-
-  # HTTParty.get('http://api.giphy.com/v1/gifs/search?q=snoop+dog&api_key=dc6zaTOxFJmzC')
-
 
   def background_picker
 		backgrounds_array = ["http://www.zlok.net/blog/wp-content/uploads/under-construction-1.gif", "http://static.ragamuffinsoul.com/wp-content/uploads/2006/12/under_construction_animated.gif", "http://www.textfiles.com/underconstruction/SouthBeachDunes3911under-construction.gif", "http://i.stack.imgur.com/tM18j.gif", "http://i.stack.imgur.com/tM18j.gif"]
